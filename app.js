cb54c7200f78ce6214bbbce37a6e71df5f166112
TJ Murphy <teej.murphy@gmail.com>
2013-03-08 14:59:41 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+(function($){
+  //console.log('foo')
+  
+  var Game = function() {
+    
+    var initialize;
+    
+    
+    
+    
+    initialize = function() {
+      
+      $('#game').addChild()
+      
+      
+      
+      
+      
+      
+    }
+    
+    
+    
+    
+    
+    
+    
+  }
+  
+  
+  
+  
+  var LaunchScene = function() {
+    
+    var initialize;
+    
+    
+    
+    initialize = function() {
+      
+      
+      
+      
+    }
+    
+    
+    
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+})(jQuery);
