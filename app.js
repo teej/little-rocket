8ea937aed5bb52a83c8f0b2e1c65dff74ed48548
sheena marie <sheena.marquez@gmail.com>
2013-03-09 19:00:49 -0800

    Changed Modal Header to Store

--- app.js
+++ app.js
@@ -263,7 +263,7 @@
       var store = $('<div id="store" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"> \
         <div class="modal-header"> \
           <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button> \
-          <h3 id="myModalLabel">Modal header</h3> \
+          <h3 id="myModalLabel">STORE</h3> \
         </div> \
         <div class="modal-body"></div> \
       </div>')
